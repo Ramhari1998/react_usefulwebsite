1332a912ce5718d443c2e8b1d72690b45f7d0faf
Ramhari1998 <poudelramhari1@gmail.com>
2022-10-08 03:25:33 -0700

    first change

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Button from "./Button"
+
+const App = ()=>{
+
+    return <div>
+     <h1> Useful Website</h1>
+     <p> Which Website Do You want to open ? </p>
+     <Button name="Google" link="https://google.com"/>
+     <Button name="Facebook" link="https://facebook.com"/>
+     <Button name="Instagram" link="https://instagram.com"/>
+     <br></br>
+     <Button name="Youtube" link="https://youtube.com"/>
+     <Button name="Github" link="https://github.com"/>
+
+     </div>
+}
+
+export default App;
